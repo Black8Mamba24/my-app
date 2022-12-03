af3a4015a963b6e96faa260f0c517d6ec01699f4
Black8Mamba24 <mateusz.mikoda23@gmail.com>
2022-12-03 11:29:25 +0100

    img

--- App.js
+++ App.js
@@ -4,21 +4,7 @@ import { StyleSheet, Text, View, Button, Alert, TouchableOpacity, TouchableHighl
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>
-				Hello <Text style={{ fontWeight: 'bold' }}>World xoxo</Text>
-			</Text>
-			<TouchableOpacity onPress={() => {}} activeOpacity={0.6}>
-				<View style={{}}>
-					<Text>Hello World</Text>
-				</View>
-			</TouchableOpacity>
-			<TouchableHighlight onPress={() => {}} activeOpacity={0.6}>
-				<View style={{}}>
-					<Text>Hello World</Text>
-				</View>
-			</TouchableHighlight>
-			<Button title='Press me' onPress={() => Alert.alert('Hello World')} />
-			<StatusBar style='auto' />
+      <Image source={require("./assets/favicon.png")} />
 		</View>
 	)
 }
