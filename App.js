ff5038c6d55c65d56ed24a098ad0a4d61287af6b
Black8Mamba24 <mateusz.mikoda23@gmail.com>
2022-12-03 14:26:32 +0100

    FlatListPhotos

--- App.js
+++ App.js
@@ -1,52 +1,33 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View, Image, Button, TouchableHighlight, Alert, TouchableOpacity } from 'react-native'
-import { FontAwesome5 } from '@expo/vector-icons'
+import { useState, useEffect } from 'react'
+import { StyleSheet, View, Text, FlatList, Image } from 'react-native'
+
+async function getPhotos() {
+	return fetch('https://jsonplaceholder.typicode.com/photos').then(response => response.json())
+}
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Image
-				source={{
-					uri: 'https://i.pinimg.com/736x/1c/be/92/1cbe92737bfba141fad4b1540f065121.jpg',
-				}}
-				style={{ width: 200, height: 200, marginTop: 50, borderRadius: 100 }}
-			/>
-			<Text style={{ fontSize: 30, marginTop: 100 }}>
-				Mateusz <Text style={{ fontWeight: 'bold' }}>Mikoda</Text>
-			</Text>
-			<StatusBar style='auto' />
-			<TouchableOpacity
-				onPress={() => {
-					Alert.alert('You clicked this button')
-				}}>
-				<View
-					style={{
-						width: 200,
-						height: 50,
-						backgroundColor: 'lightgray',
-						borderRadius: 10,
-						marginTop: 20,
-						display: 'flex',
-						flexDirection: 'row',
-						justifyContent: 'center',
-						alignItems: 'center',
-					}}>
-					<FontAwesome5 name='cube' size={24} color='black' />
-					<Text style={{ marginLeft: 10 }}>Click this icon!</Text>
-				</View>
-			</TouchableOpacity>
+	const [photos, setPhotos] = useState([])
 
-			<Image
-				source={require('./assets/favicon.png')}
-				style={{
-					width: 100,
-					height: 100,
-					marginTop: 10,
-					marginBottom: 50,
-					borderRadius: 100,
-				}}
-			/>
+	useEffect(() => {
+		getPhotos().then(photos => setPhotos(photos))
+	}, [])
+
+	const itemRenderer = ({ item, index }) => (
+		<View style={[[styles.listItemContainer, index % 2 === 0 ? styles.odd : {}]]}>
+			<Image style={styles.image} source={{ uri: `${item.thumbnailUrl}.png` }} />
+			<Text>{item.title}</Text>
 		</View>
+	)
+
+	return (
+		<FlatList
+			ListHeaderComponent={() => <Text style={styles.header}>Photos</Text>}
+			ListFooterComponent={() => <Text style={styles.footer}>End</Text>}
+			ListEmptyComponent={() => <Text style={styles.empty}>Empty</Text>}
+			data={photos}
+			keyExtractor={item => item.id}
+			renderItem={itemRenderer}
+		/>
 	)
 }
 
@@ -54,7 +35,24 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
 		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	header: {
+		paddingTop: 20,
+		fontWeight: 'bold',
+		fontSize: 20,
+		textAlign: 'center',
+	},
+	image: {
+		width: 150,
+		height: 150,
+	},
+	listItemContainer: {
+		paddingVertical: 10,
+		alignItems: 'center',
+	},
+	odd: {
+		backgroundColor: 'rgba(194, 249, 112, 0.2)',
 	},
 })
