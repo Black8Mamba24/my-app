f44f789e5a8da83f9fd795e86e8bb3526819aae7
Black8Mamba24 <mateusz.mikoda23@gmail.com>
2022-12-03 11:33:34 +0100

    img uri

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View, Button, Alert, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback, Image } from 'react-native'
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	Alert,
+	TouchableOpacity,
+	TouchableHighlight,
+	TouchableNativeFeedback,
+	Image,
+} from 'react-native'
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-      <Image source={require("./assets/favicon.png")} />
+			<Image
+				style={{
+					width: 100,
+					height: 100,
+				}}
+				source={{
+					uri: 'https://i.pinimg.com/736x/1c/be/92/1cbe92737bfba141fad4b1540f065121.jpg',
+				}}
+			/>
 		</View>
 	)
 }
