e630b80e3fc2570e4ab038901520cecc06831dd0
Black8Mamba24 <mateusz.mikoda23@gmail.com>
2022-12-03 11:11:35 +0100

    button

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Button, Alert } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>
+				Hello <Text style={{ fontWeight: 'bold' }}>World</Text>
+			</Text>
+			<Button title='Press me' onPress={() => Alert.alert('Hello World')} />
+			<StatusBar style='auto' />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
