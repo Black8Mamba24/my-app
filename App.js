f2b2347a2b8626c5e005c346c30912ad0b05085f
Black8Mamba24 <mateusz.mikoda23@gmail.com>
2022-12-03 11:26:34 +0100

    opacity

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View, Button, Alert } from 'react-native'
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback, Image } from 'react-native'
 
 export default function App() {
 	return (
 		<View style={styles.container}>
 			<Text>
-				Hello <Text style={{ fontWeight: 'bold' }}>World</Text>
+				Hello <Text style={{ fontWeight: 'bold' }}>World xoxo</Text>
 			</Text>
+			<TouchableOpacity onPress={() => {}} activeOpacity={0.6}>
+				<View style={{}}>
+					<Text>Hello World</Text>
+				</View>
+			</TouchableOpacity>
+			<TouchableHighlight onPress={() => {}} activeOpacity={0.6}>
+				<View style={{}}>
+					<Text>Hello World</Text>
+				</View>
+			</TouchableHighlight>
 			<Button title='Press me' onPress={() => Alert.alert('Hello World')} />
 			<StatusBar style='auto' />
 		</View>
