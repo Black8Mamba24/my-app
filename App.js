bb104b0931211f64bb14df1e95074390bc9bb67a
Black8Mamba24 <mateusz.mikoda23@gmail.com>
2022-12-03 14:33:49 +0100

    valueInput

--- App.js
+++ App.js
@@ -1,33 +1,14 @@
-import { useState, useEffect } from 'react'
-import { StyleSheet, View, Text, FlatList, Image } from 'react-native'
-
-async function getPhotos() {
-	return fetch('https://jsonplaceholder.typicode.com/photos').then(response => response.json())
-}
+import { useState } from 'react'
+import { StyleSheet, View, Text, TextInput } from 'react-native'
 
 export default function App() {
-	const [photos, setPhotos] = useState([])
-
-	useEffect(() => {
-		getPhotos().then(photos => setPhotos(photos))
-	}, [])
-
-	const itemRenderer = ({ item, index }) => (
-		<View style={[[styles.listItemContainer, index % 2 === 0 ? styles.odd : {}]]}>
-			<Image style={styles.image} source={{ uri: `${item.thumbnailUrl}.png` }} />
-			<Text>{item.title}</Text>
-		</View>
-	)
+	const [value, setValue] = useState('')
 
 	return (
-		<FlatList
-			ListHeaderComponent={() => <Text style={styles.header}>Photos</Text>}
-			ListFooterComponent={() => <Text style={styles.footer}>End</Text>}
-			ListEmptyComponent={() => <Text style={styles.empty}>Empty</Text>}
-			data={photos}
-			keyExtractor={item => item.id}
-			renderItem={itemRenderer}
-		/>
+		<View style={styles.container}>
+			<Text>Value: {value}</Text>
+			<TextInput style={styles.input} onChangeText={text => setValue(text)} />
+		</View>
 	)
 }
 
@@ -38,21 +19,11 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
-	header: {
-		paddingTop: 20,
-		fontWeight: 'bold',
-		fontSize: 20,
-		textAlign: 'center',
-	},
-	image: {
-		width: 150,
-		height: 150,
-	},
-	listItemContainer: {
-		paddingVertical: 10,
-		alignItems: 'center',
-	},
-	odd: {
-		backgroundColor: 'rgba(194, 249, 112, 0.2)',
+	input: {
+		backgroundColor: '#eee',
+		paddingVertical: 8,
+		paddingHorizontal: 16,
+		width: '50%',
+		borderRadius: 4,
 	},
 })
