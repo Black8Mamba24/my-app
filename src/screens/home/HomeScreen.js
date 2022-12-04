a944a4f807c98a160319bbb46665feae90fff287
Black8Mamba24 <mateusz.mikoda23@gmail.com>
2022-12-04 12:19:08 +0100

    HomeAddAnimationLottie

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,9 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Button, Alert } from 'react-native'
+import Lottie from 'lottie-react-native'
 
 export default function HomeScreen() {
 	return (
 		<View>
-			<Text>HomeScreen</Text>
+			<Lottie source={require('../../../assets/owl.json')} autoPlay loop style={{ width: 300, height: 200 }} />
 		</View>
 	)
 }
